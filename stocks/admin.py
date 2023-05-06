de2ffb3fec66a5c02744cc676a6c8de3299b0787
Eric_Spychalski <EricSpychalski@gmail.COM>
2023-05-06 10:34:27 -0600

    setting up models :wire:

--- stocks/admin.py
+++ stocks/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Company, History
 
 # Register your models here.
+
+admin.site.register([Company, History])
--- stocks/models.py
+++ stocks/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Company(models.Model):
+    name = models.CharField(max_length=100)
+    ticker = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
+class History(models.Model):
+    ticker = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='history')
+    date = models.DateField()
+    open = models.IntegerField()
+    close = models.IntegerField()
+    dividends = models.IntegerField()
+    splits = models.IntegerField()
+
+    def __str__(self):
+        return self.ticker

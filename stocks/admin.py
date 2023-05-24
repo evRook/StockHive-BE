c450476174522cdfea53cbf854f3db6b2c0be9de
Eric_Spychalski <EricSpychalski@gmail.COM>
2023-05-24 09:46:17 -0600

    adding favorites to admin

--- stocks/admin.py
+++ stocks/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import CompanyInfo, History, UserAcct, UserAcctManager
+from .models import CompanyInfo, History, UserAcct, UserAcctManager, UserFavorites
 from . import models
 from import_export.admin import ImportExportModelAdmin
 
@@ -12,3 +12,4 @@ admin.site.register(models.CompanyInfo, CompanyAdmin)
 
 admin.site.register(UserAcct)
 admin.site.register(History)
+admin.site.register(UserFavorites)
